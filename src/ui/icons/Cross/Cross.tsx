8bd2ca6fc7c1e184af5d2473a30fa4c22ebbc111
Михаил <etozhenerk@yandex.ru>
2025-06-25 23:11:10 +0300

    fix: fix icons

--- src/ui/icons/Cross/Cross.tsx
+++ src/ui/icons/Cross/Cross.tsx
@@ -1,15 +1,16 @@
 import { FC } from 'react';
 
 import { IconBaseProps } from '@app-types/common';
+import { SvgBase } from '@ui/SvgBase';
 
-export const Cross: FC<IconBaseProps> = ({ color, size, ...rest }) => (
-    <svg width={size} height={size} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
+export const Cross: FC<IconBaseProps> = ({ size, ...rest }) => (
+    <SvgBase size={size} {...rest}>
         <path
             d="M6.66675 25.3332L16.0001 15.9998M16.0001 15.9998L25.3334 6.6665M16.0001 15.9998L6.66675 6.6665M16.0001 15.9998L25.3334 25.3332"
-            stroke={color ?? 'currentColor'}
+            stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
         />
-    </svg>
+    </SvgBase>
 );
